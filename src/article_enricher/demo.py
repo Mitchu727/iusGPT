cefe01e83bcae6321c879f97ee61606f543ff460
Mitchu 727 <michal.mmtk@gmail.com>
2024-02-23 00:09:44 +0100

    Implement simple article enricher

--- src/article_enricher/demo.py
+++ src/article_enricher/demo.py
@@ -1,35 +1,49 @@
-from llama_index.vector_stores import ChromaVectorStore
 from src.article_collection.chroma_db_collection import get_civil_code_collection
-from llama_index import SimpleDirectoryReader, VectorStoreIndex, ServiceContext, StorageContext, load_index_from_storage
 
 from src.secrets import OPEN_API_KEY
-from src.article_collection.embeddings import sentence_transformer
-
 import os
+import chromadb
+from openai import OpenAI
 
-PROMPT = """
-    Dane jest następujące pytanie:
-    Czym jest gospodarstwo rolne?
+QUERY = "Czym jest gospodarstwo rolne?"
 
-    Twoim zadaniem jest wybrać z podanych artykułów te, które mogą być przydatne i uzupełnić nimi pytanie.
-    Udziel odpowiedzi w formie:
-    <zagadnienie>
-    <artykuł>
+def prompt_wrapper(query, articles):
+    return f"""
+Dane jest następujące pytanie:
+{query}
+
+I następujące przepisy prawne
+{articles}
+
+Twoim zadaniem jest wybrać z podanych artykułów te, które mogą być przydatne i uzupełnić nimi pytanie.
+Udziel odpowiedzi w formie:
+<zagadnienie>
+<artykuł>
 """
 
 os.environ["OPENAI_API_KEY"] = OPEN_API_KEY
 
 if __name__ == "__main__":
-    print(len(sentence_transformer(PROMPT)))
     chroma_collection = get_civil_code_collection()
-    vector_store = ChromaVectorStore(chroma_collection=chroma_collection, dimension=384)
-    index = VectorStoreIndex.from_vector_store(
-        vector_store=vector_store,
-        embed_model=sentence_transformer,
-        dimension = 384
+    results = chroma_collection.query(query_texts=[QUERY], n_results=10)
+    articles_list = results['documents'][0]
+    articles = "".join(articles_list)
+    prompt = prompt_wrapper(QUERY, articles)
+    print(prompt)
+    client = OpenAI(
+        # This is the default and can be omitted
+        api_key=os.environ.get("OPENAI_API_KEY"),
     )
 
-    query_engine = index.as_query_engine()
-    response = query_engine.query(PROMPT)
+    response = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ],
+        model="gpt-3.5-turbo",
+    )
+    print(response.choices[0].message.content)
 
     # print(response)
