e69847ae5025852cfeb41a77f262ca74efd5d9ef
Mitchu 727 <michal.mmtk@gmail.com>
2024-03-15 00:53:03 +0100

    naive implementation of multiple agents

--- src/main.py
+++ src/main.py
@@ -19,7 +19,7 @@ model_name = "sdadas/mmlw-roberta-base"
 embedding_function = SentenceTransformerEmbeddings(model_name=model_name)
 # model_name = "all-MiniLM-L6-v2"
 persist_directory = "../build"
-create_new_collection = True
+create_new_collection = False
 
 if create_new_collection:
     civil_code_path = "../documents/civilCodeTxtGenerated/kodeks.txt"
@@ -42,36 +42,102 @@ else:
 
 print("There are", db._collection.count(), "documents in the collection")
 
-query = "Czym jest gospodarstwo rolne?"
-docs = db.similarity_search(query, k=10)
-for doc in docs:
-    print(doc.page_content)
+# query = "Czym jest gospodarstwo rolne?"
+# docs = db.similarity_search(query, k=10)
+# for doc in docs:
+#     print(doc.page_content)
 
+# rewrite_query_template = """Given the following question:
+# {question}
+#
+# What are the concepts that should be checked in civil code to answer this question?
+# """
+
+# =====RETRIEVER=====
 # retriever
-retriever = db.as_retriever()
+retriever = db.as_retriever(search_kwargs={"k": 10})
+retriever_prompt_template = """Given the following question:
+{question}
+And some articles:
 
-# template = """Answer the question based only on the following context:
-# {context}
+<context>
+{context}
+</context>
+
+Decide which articles from context are relevant to the following question.
+In the answer you should only type these articles with full text of them.
+
+"""
+
+# template = """Use the following pieces of context to answer the question at the end. In an answer you should mention
+# article from context.
+# If you don't know the answer, just say that you don't know, don't try to make up an answer.
+# Use three sentences maximum and keep the answer as concise as possible.
 #
-#
-# Decide which articles from context are relevant to the following question:
 # {context}
 #
 # Question: {question}
-#
-# In the answer you should only type the given question and articles related to it.
-#
 # """
 
-template = """Use the following pieces of context to answer the question at the end. In an answer you should mention 
-article from context. 
-If you don't know the answer, just say that you don't know, don't try to make up an answer.
-Use three sentences maximum and keep the answer as concise as possible.
+retriever_prompt = ChatPromptTemplate.from_template(retriever_prompt_template)
+model = ChatOpenAI()
 
-{context}
+retriever_chain = (
+    {"context": retriever, "question": RunnablePassthrough()}
+    | retriever_prompt
+    | model
+    | StrOutputParser()
+)
+# =====PRO_AGENT=====
 
-Question: {question}
+pro_agent_prompt_template = """Given the following articles from civil code:
+{articles}
+
+Answer given question: {question}. In the answer cite the proper article
 """
+
+pro_agent_prompt = ChatPromptTemplate.from_template(pro_agent_prompt_template)
+
+pro_agent_chain = (
+    # {"context": retriever_chain, "question": itemgetter("question")}
+    pro_agent_prompt
+    | model
+    | StrOutputParser()
+)
+
+# =====CONTRA_AGENT=====
+
+contra_agent_prompt_template = """There was a question: {question}
+Some person provided following answer: {answer} based on following articles {articles}
+You should find and write down any mistakes this person made.
+"""
+
+contra_agent_prompt = ChatPromptTemplate.from_template(contra_agent_prompt_template)
+
+contra_agent_chain = (
+    # {"context": retriever_chain, "question": itemgetter("question")}
+    contra_agent_prompt
+    | model
+    | StrOutputParser()
+)
+
+# =====SUMMARY_AGENT=====
+
+summary_agent_prompt_template = """There was a following question: {question} and the articles related to it {articles}.
+One person answered it: {pro_response}
+However some other person had some remarks: {contra_response}
+Provide answer for given question based on those two opinions and related articles.
+"""
+
+summary_agent_prompt = ChatPromptTemplate.from_template(summary_agent_prompt_template)
+
+summary_agent_chain = (
+    # {"context": retriever_chain, "question": itemgetter("question")}
+    summary_agent_prompt
+    | model
+    | StrOutputParser()
+)
+
 
 queries = [
     "Czym jest gospodarstwo rolne?",
@@ -81,30 +147,29 @@ queries = [
     "Co wchodzi w skład przedsiębiorstwa?",
     "Jakie skutki wywołuje czynność prawna",
     "Jak może być wyrażone oświadczenie woli?",
-    "Kiedy czynność prawna jest nieważna",
+    "Kiedy czynność prawna jest nieważna?",
     "Czym jest prokura?",
     "Kiedy kończy się termin oznaczony w dniach?"
 ]
 
-prompt = ChatPromptTemplate.from_template(template)
-model = ChatOpenAI()
 
-chain = (
-    {"context": retriever, "question": RunnablePassthrough()}
-    | prompt
-    | model
-    | StrOutputParser()
-)
 experiments = []
 for query in queries:
-    response = chain.invoke(query)
+    retrieved_documents = retriever_chain.invoke(query)
+    pro_response = pro_agent_chain.invoke({"articles": retrieved_documents, "question": query})
+    contra_response = contra_agent_chain.invoke({"question": query, "answer": pro_response, "articles": retrieved_documents})
+    summary_response = summary_agent_chain.invoke({"question": query, "articles": retrieved_documents, "pro_response": pro_response, "contra_response": contra_response})
+
     case = {}
     case["query"] = query
-    case["answer"] = response
+    case["retrieved_documents"] = retrieved_documents
+    case["pro_response"] = pro_response
+    case["contra_response"] = contra_response
+    case["summary_response"] = summary_response
     print("<-PYTANIE->")
     print(query)
     print("<-ODPOWIEDŹ->")
-    print(response)
+    print(summary_response)
     experiments.append(case.copy())
 
 with open('experiments.json', 'w') as f:
--- src/utils/formatter.py
+++ src/utils/formatter.py
@@ -1,31 +1,7 @@
 import json
 from openpyxl import Workbook
-
+import pandas as pd
 
 def from_json_to_xlsx(json_file, xlsx_file):
-
-    with open(json_file) as f:
-        experiments = json.load(f)
-
-    workbook = Workbook()
-    sheet = workbook.active
-    sheet["A1"] = "Question"
-    sheet["B1"] = "Answer"
-
-    index = 2
-    for experiment in experiments:
-        sheet["A"+str(index)] = experiment['query']
-        sheet["B"+str(index)] = experiment['answer']
-        index += 1
-
-    workbook.save(xlsx_file)
-
-    #     markdown_string += f"|{experiment['query']}|{experiment['answer']}|\n"
-    # print(markdown_string)
-
-
-
-#     markdown_string = """
-# | Question    | Answer |
-# | -------- | ------- |
-# """
+    experiments = pd.read_json(json_file)
+    experiments.to_excel(xlsx_file)
