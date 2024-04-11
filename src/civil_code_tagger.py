e96d75876e4c59888970410a259490fd84fc56d4
Mitchu 727 <michal.mmtk@gmail.com>
2024-04-11 19:03:36 +0200

    recursive tagging, failed

--- src/civil_code_tagger.py
+++ src/civil_code_tagger.py
@@ -1,7 +1,7 @@
 import json
 input_path = "../documents/civilCodeSplitted/kodeks.json"
 cache_path = "../documents/civilCodeSplitted/cache.json"
-output_path = "../documents/civilCodeSplitted/codex_tagged.json"
+output_path = "../documents/civilCodeSplitted/articles_tagged.json"
 from src.tagger import DummyTagger, ChatGPT35Tagger
 import itertools
 
@@ -17,16 +17,30 @@ with open(input_path, "r") as f:
 # article_tagger = DummyArticleTagger()
 tagger = ChatGPT35Tagger()
 
-# print(chapter_one)
-for chapter in codex:
-    content = chapter["id"] + "\n" +  "".join(chapter["articles"])
-    tags = tagger.tag_text(content)
-    chapter["chapter_tag_summarized"] = tags
-    with open(cache_path, "a") as f:
-        json.dump(chapter, f)
-    print(chapter["id"])
-    print(tags)
-# print(article_tags)
+results = []
+for chapter in codex[:2]:
+    for article in chapter["articles"]:
+        result = {
+            article: tagger.tag_text(article)
+        }
+        results.append(result)
+
+print(len(tagger.get_tags()))
+print(tagger.get_tags())
 
 with open(output_path, "w") as f:
-    json.dump(codex, f)
+    json.dump(results, f)
+
+# # print(chapter_one)
+# for chapter in codex:
+#     content = chapter["id"] + "\n" +  "".join(chapter["articles"])
+#     tags = tagger.tag_text(content)
+#     chapter["chapter_tag_summarized"] = tags
+#     with open(cache_path, "a") as f:
+#         json.dump(chapter, f)
+#     print(chapter["id"])
+#     print(tags)
+# # print(article_tags)
+#
+# with open(output_path, "w") as f:
+#     json.dump(codex, f)
--- src/tagger.py
+++ src/tagger.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+
+from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage, SystemMessage
 import os
@@ -19,14 +21,43 @@ class DummyTagger(Tagger):
 class ChatGPT35Tagger(Tagger):
     def __init__(self):
         self.chat = ChatOpenAI(model="gpt-3.5-turbo-0125", openai_api_key=OPEN_API_KEY)
-        self.system_message = SystemMessage(content="You're an expert in polish civil law. Your job is to tag articles that you get with "
-                                                    "the terms or ideas they are related to it. Response in polish. Do not use large letters and dots"
+        self.system_message = SystemMessage(content="You're an expert in polish civil law. You will receive the articles from polish civil code."
+                                                    "Your job is to tag them with terms, concepts and ideas they are related to. Possible tags can be provided with a question, "
+                                                    "but you can also come up new tags. If there are typos in article fix them."
+                                                    "Response in polish. Do not use large letters and dots"
                                                     "In the response write only the tags separated with commas")
+        self.tags = []
+        self.prompt_template = PromptTemplate.from_template(
+            "Article: {article}"
+            "Tags: {tags}"
+        )
+
     def tag_text(self, article: str) -> list[str]:
-        human_message = HumanMessage(content=article)
+        prompt = self.prompt_template.invoke({"article": article, "tags": self.get_tags_as_string()}).to_string()
+        human_message = HumanMessage(content=prompt)
         messages = [
             human_message,
             self.system_message
         ]
         response = self.chat.invoke(messages)
-        return response.content.split(", ")
+        tags = self.parse_response(response)
+        print(tags)
+        self.update_tags(tags)
+        return tags
+
+    def update_tags(self, tags: list[str]):
+        for tag in tags:
+            if tag not in self.tags:
+                self.tags.append(tag)
+
+    def parse_response(self, response: str) -> list[str]:
+        parsed_response = response.content.split(", ")
+        if "." in parsed_response[-1]:
+            parsed_response[-1] = parsed_response[-1][:-1]
+        return parsed_response
+
+    def get_tags(self) -> list[str]:
+        return self.tags
+
+    def get_tags_as_string(self) -> str:
+        return ",".join(self.tags)
