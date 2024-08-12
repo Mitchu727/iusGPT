c02d170d9c853de3517afb03942eba2d968b9368
Mitchu 727 <michal.mmtk@gmail.com>
2024-08-13 00:15:07 +0200

    Refactor ingesting code a little bit

--- documents/evaluation/civil_law_exam/ingest.py
+++ documents/evaluation/civil_law_exam/ingest.py
@@ -4,91 +4,51 @@ from src.utils.utils import get_project_root
 civil_questions = []
 civil_answers = []
 
-## ROK 2022
-## egzamin adwokacko-radcowski
-chosen_dir = get_project_root() / "documents" / "evaluation" / "egzamin_wstepny_adwokacki_radcowski_2022"
-questions_path = chosen_dir / "questions.json"
-answers_path = chosen_dir / "answers.json"
+def get_civil_questions_and_answers_from_directory(directory, lower_index, upper_index):
+    chosen_dir = get_project_root() / "documents" / "evaluation" / directory
+    questions_path = chosen_dir / "questions.json"
+    answers_path = chosen_dir / "answers.json"
 
-with open(questions_path, 'r') as f:
-    questions = json.load(f)
+    with open(questions_path, 'r') as f:
+        questions_from_file = json.load(f)
 
-with open(answers_path, 'r') as f:
-    answers = json.load(f)
+    with open(answers_path, 'r') as f:
+        answers_from_file = json.load(f)
 
-civil_questions.extend(questions[29:50])
-civil_answers.extend(answers[29:50])
+    return questions_from_file[lower_index:upper_index], answers_from_file[lower_index:upper_index]
 
-## egzamin notarialny
-chosen_dir = get_project_root() / "documents" / "evaluation" / "egzamin_wstepny_notarialny_2022"
-questions_path = chosen_dir / "questions.json"
-answers_path = chosen_dir / "answers.json"
-
-with open(questions_path, 'r') as f:
-    questions = json.load(f)
-
-with open(answers_path, 'r') as f:
-    answers = json.load(f)
-
-civil_questions.extend(questions[0:30])
-civil_answers.extend(answers[0:30])
-
-## egzamin komorniczy
-chosen_dir = get_project_root() / "documents" / "evaluation" / "egzamin_wstepny_komorniczy_2022"
-questions_path = chosen_dir / "questions.json"
-answers_path = chosen_dir / "answers.json"
-
-with open(questions_path, 'r') as f:
-    questions = json.load(f)
-
-with open(answers_path, 'r') as f:
-    answers = json.load(f)
-
-civil_questions.extend(questions[9:34])
-civil_answers.extend(answers[9:34])
 
 ## ROK 2022
 ## egzamin adwokacko-radcowski
-chosen_dir = get_project_root() / "documents" / "evaluation" / "egzamin_wstepny_adwokacki_radcowski_2023"
-questions_path = chosen_dir / "questions.json"
-answers_path = chosen_dir / "answers.json"
-
-with open(questions_path, 'r') as f:
-    questions = json.load(f)
-
-with open(answers_path, 'r') as f:
-    answers = json.load(f)
-
-civil_questions.extend(questions[28:45])
-civil_answers.extend(answers[28:45])
+questions, answers = get_civil_questions_and_answers_from_directory("egzamin_wstepny_adwokacki_radcowski_2022", 29, 50)
+civil_questions.extend(questions)
+civil_answers.extend(answers)
 
 ## egzamin notarialny
-chosen_dir = get_project_root() / "documents" / "evaluation" / "egzamin_wstepny_notarialny_2023"
-questions_path = chosen_dir / "questions.json"
-answers_path = chosen_dir / "answers.json"
-
-with open(questions_path, 'r') as f:
-    questions = json.load(f)
-
-with open(answers_path, 'r') as f:
-    answers = json.load(f)
-
-civil_questions.extend(questions[0:29])
-civil_answers.extend(answers[0:29])
+questions, answers = get_civil_questions_and_answers_from_directory("egzamin_wstepny_notarialny_2022", 0, 30)
+civil_questions.extend(questions)
+civil_answers.extend(answers)
 
 ## egzamin komorniczy
-chosen_dir = get_project_root() / "documents" / "evaluation" / "egzamin_wstepny_komorniczy_2023"
-questions_path = chosen_dir / "questions.json"
-answers_path = chosen_dir / "answers.json"
+questions, answers = get_civil_questions_and_answers_from_directory("egzamin_wstepny_komorniczy_2022", 9, 34)
+civil_questions.extend(questions)
+civil_answers.extend(answers)
 
-with open(questions_path, 'r') as f:
-    questions = json.load(f)
+## ROK 2023
+## egzamin adwokacko-radcowski
+questions, answers = get_civil_questions_and_answers_from_directory("egzamin_wstepny_adwokacki_radcowski_2023", 28, 45)
+civil_questions.extend(questions)
+civil_answers.extend(answers)
 
-with open(answers_path, 'r') as f:
-    answers = json.load(f)
+## egzamin notarialny
+questions, answers = get_civil_questions_and_answers_from_directory("egzamin_wstepny_notarialny_2023", 0, 29)
+civil_questions.extend(questions)
+civil_answers.extend(answers)
 
-civil_questions.extend(questions[11:35])
-civil_answers.extend(answers[11:35])
+## egzamin komorniczy
+questions, answers = get_civil_questions_and_answers_from_directory("egzamin_wstepny_komorniczy_2023", 11, 35)
+civil_questions.extend(questions)
+civil_answers.extend(answers)
 
 print(len(civil_questions))
 
